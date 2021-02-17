2e5ba1f072c99d3d5122c193ee89814c20abd81c
Geoffrey Jost <geoffrey@jostwebwerks.com>
2021-02-17 13:33:46 -0800

    Far/WraRecord and Person from namesdb-editor, first pass

--- namesdb_public/models.py
+++ namesdb_public/models.py
@@ -10,8 +10,6 @@ import elasticsearch_dsl as dsl
 
 from . import definitions
 
-DOC_TYPE = 'names-record'
-
 
 def _hitvalue(hit, field):
     """Extract list-wrapped values from their lists.
@@ -32,63 +30,8 @@ def _hitvalue(hit, field):
 
 
 class Record(dsl.Document):
-    """FAR/WRA record model
-    
-    m_pseudoid = m_camp + lastname + birthyear + firstname
+    """Base record type
     """
-    m_pseudoid = dsl.Keyword()
-    m_dataset = dsl.Keyword()
-    m_camp = dsl.Keyword()
-    m_lastname = dsl.Text()
-    m_firstname = dsl.Text()
-    m_birthyear = dsl.Keyword()
-    m_gender = dsl.Keyword()
-    m_familyno = dsl.Keyword()
-    m_individualno = dsl.Keyword()
-    m_originalstate = dsl.Keyword()
-    errors = dsl.Text()
-    
-    f_originalcity = dsl.Keyword()
-    f_othernames = dsl.Keyword()
-    f_maritalstatus = dsl.Keyword()
-    f_citizenship = dsl.Keyword()
-    f_alienregistration = dsl.Keyword()
-    f_entrytype = dsl.Keyword()
-    f_entrydate = dsl.Date()
-    f_departuretype = dsl.Keyword()
-    f_departuredate = dsl.Date()
-    f_destinationstate = dsl.Keyword()
-    f_destinationcity = dsl.Keyword()
-    f_campaddress = dsl.Keyword()
-    f_farlineid = dsl.Keyword()
-    
-    w_assemblycenter = dsl.Keyword()
-    w_originaladdress = dsl.Keyword()
-    w_birthcountry = dsl.Keyword()
-    w_fatheroccup = dsl.Keyword()
-    w_fatheroccupcat = dsl.Keyword()
-    w_yearsschooljapan = dsl.Keyword()
-    w_gradejapan = dsl.Keyword()
-    w_schooldegree = dsl.Keyword()
-    w_yearofusarrival = dsl.Keyword()
-    w_timeinjapan = dsl.Keyword()
-    w_notimesinjapan = dsl.Keyword()
-    w_ageinjapan = dsl.Keyword()
-    w_militaryservice = dsl.Keyword()
-    w_maritalstatus = dsl.Keyword()
-    w_ethnicity = dsl.Keyword()
-    w_birthplace = dsl.Keyword()
-    w_citizenshipstatus = dsl.Keyword()
-    w_highestgrade = dsl.Keyword()
-    w_language = dsl.Keyword()
-    w_religion = dsl.Keyword()
-    w_occupqual1 = dsl.Keyword()
-    w_occupqual2 = dsl.Keyword()
-    w_occupqual3 = dsl.Keyword()
-    w_occuppotn1 = dsl.Keyword()
-    w_occuppotn2 = dsl.Keyword()
-    w_filenumber = dsl.Keyword()
-    
     fulltext = dsl.Text()  # see Record.assemble_fulltext()
     
     #class Index:
@@ -96,22 +39,12 @@ class Record(dsl.Document):
     # We could define Index here but we don't because we want to be consistent
     # with ddr-local and ddr-public.
     
-    class Meta:
-        doc_type = DOC_TYPE
-    
-    def __repr__(self):
-        return "<Record %s>" % Record.make_id(self.m_dataset, self.m_pseudoid)
-
     @staticmethod
-    def make_id(m_dataset, m_pseudoid):
-        return ':'.join([m_dataset, m_pseudoid])
-    
-    @staticmethod
-    def from_dict(fieldnames, m_dataset, m_pseudoid, data):
+    def from_dict(class_, fieldnames, record_id, data):
         """
+        @param class_: Person, FarRecord, WraRecord
         @param fieldnames: list
-        @param m_dataset: str
-        @param m_pseudoid: str
+        @param record_id: str
         @param data: dict
         @returns: Record
         """
@@ -123,8 +56,8 @@ class Record(dsl.Document):
         for fieldname in empty_dates:
             data.pop(fieldname)
         # set values
-        record = Record(meta={
-            'id': Record.make_id(m_dataset, m_pseudoid)
+        record = class_(meta={
+            'id': record_id
         })
         record.errors = []
         for field in fieldnames:
@@ -134,40 +67,37 @@ class Record(dsl.Document):
                 except dsl.exceptions.ValidationException:
                     err = ':'.join([field, data[field]])
                     record.errors.append(err)
-        record.m_dataset = m_dataset
         record.assemble_fulltext()
         return record
     
     @staticmethod
-    def from_hit(hit):
+    def from_hit(class_, hit):
         """Build Record object from Elasticsearch hit
+        @param class_: Person, FarRecord, WraRecord
         @param hit
         @returns: Record
         """
         hit_d = hit.__dict__['_d_']
-        m_pseudoid = _hitvalue(hit_d, 'm_pseudoid')
-        m_dataset = _hitvalue(hit_d, 'm_dataset')
-        if m_dataset and m_pseudoid:
-            record = Record(meta={
-                'id': Record.make_id(m_dataset, m_pseudoid)
+        if record_id:
+            record = class_(meta={
+                'id': record_id
             })
             for field in definitions.FIELDS_MASTER:
                 setattr(record, field, _hitvalue(hit_d, field))
-            record.m_dataset = m_dataset
             record.assemble_fulltext()
             return record
         record.assemble_fulltext()
         return None
      
     @staticmethod
-    def field_values(field, es=None, index=None):
+    def field_values(class_, field, es=None, index=None):
         """Returns unique values and counts for specified field.
         """
         if es and index:
             s = dsl.Search(using=es, index=index)
         else:
             s = dsl.Search()
-        s = s.doc_type(Record)
+        s = s.doc_type(class_)
         s.aggs.bucket('bucket', 'terms', field=field, size=1000)
         response = s.execute()
         return [
@@ -175,63 +105,262 @@ class Record(dsl.Document):
             for x in response.aggregations['bucket']['buckets']
         ]
         
+    def assemble_fulltext(self, fieldnames):
+        """Assembles single fulltext search field from all string fields
+        """
+        values = []
+        for fieldname in fieldnames:
+            value = getattr(self, fieldname, None)
+            if value:
+                if isinstance(value, str):
+                    value = value.lower()
+                values.append(value)
+        return ' '.join(values)
+
+
+FIELDS_PERSON = [
+    'nr_id', 'family_name', 'given_name', 'given_name_alt', 'other_names',
+    'middle_name', 'prefix_name', 'suffix_name', 'jp_name', 'preferred_name',
+    'birth_date', 'birth_date_text', 'birth_place', 'death_date',
+    'death_date_text', 'wra_family_no', 'wra_individual_no', 'citizenship',
+    'alien_registration_no', 'gender', 'preexclusion_residence_city',
+    'preexclusion_residence_state', 'postexclusion_residence_city',
+    'postexclusion_residence_state', 'exclusion_order_title',
+    'exclusion_order_id', 'timestamp', 'facility',
+]
+
+class Person(Record):
+    """Person record model
+    """
+    nr_id                         = dsl.Keyword()
+    family_name                   = dsl.Keyword()
+    given_name                    = dsl.Keyword()
+    given_name_alt                = dsl.Text()
+    other_names                   = dsl.Text()
+    middle_name                   = dsl.Keyword()
+    prefix_name                   = dsl.Keyword()
+    suffix_name                   = dsl.Keyword()
+    jp_name                       = dsl.Keyword()
+    preferred_name                = dsl.Keyword()
+    birth_date                    = dsl.Date()
+    birth_date_text               = dsl.Keyword()
+    birth_place                   = dsl.Keyword()
+    death_date                    = dsl.Date()
+    death_date_text               = dsl.Keyword()
+    wra_family_no                 = dsl.Keyword()
+    wra_individual_no             = dsl.Keyword()
+    citizenship                   = dsl.Keyword()
+    alien_registration_no         = dsl.Keyword()
+    gender                        = dsl.Keyword()
+    preexclusion_residence_city   = dsl.Keyword()
+    preexclusion_residence_state  = dsl.Keyword()
+    postexclusion_residence_city  = dsl.Keyword()
+    postexclusion_residence_state = dsl.Keyword()
+    exclusion_order_title         = dsl.Keyword()
+    exclusion_order_id            = dsl.Keyword()
+    #timestamp                     = dsl.DateTime()
+    #facility = models.ManyToManyField(Facility, through='PersonFacility')
+    
+    def __repr__(self):
+        return f'<Person {self.nr_id}>'
+    
+    @staticmethod
+    def from_dict(fieldnames, nr_id, data):
+        """
+        @param fieldnames: list
+        @param nr_id: str
+        @param data: dict
+        @returns: Person
+        """
+        return Record.from_dict(Person, fieldnames, nr_id, data)
+    
+    @staticmethod
+    def from_hit(hit):
+        """Build Person object from Elasticsearch hit
+        @param hit
+        @returns: Person
+        """
+        return Record.from_hit(Person, hit)
+     
+    @staticmethod
+    def field_values(field, es=None, index=None):
+        """Returns unique values and counts for specified field.
+        """
+        return Record.field_values(Person, field, es, index)
+   
     def assemble_fulltext(self):
         """Assembles single fulltext search field from all string fields
         """
-        fields = [
-            self.m_pseudoid,
-            self.m_dataset,
-            self.m_camp,
-            self.m_lastname,
-            self.m_firstname,
-            self.m_birthyear,
-            self.m_gender,
-            self.m_familyno,
-            self.m_individualno,
-            self.m_originalstate,
-            self.f_originalcity,
-            self.f_othernames,
-            self.f_maritalstatus,
-            self.f_citizenship,
-            self.f_alienregistration,
-            self.f_entrytype,
-            self.f_entrydate,
-            #self.f_entrydate.isoformat(),              # multiple data formats
-            #self.f_entrydate.strftime('%d-%m-%Y'),     #
-            #self.f_entrydate.strftime('%m-%d-%Y'),     #
-            self.f_departuretype,
-            self.f_departuredate,
-            self.f_destinationstate,
-            self.f_destinationcity,
-            self.f_campaddress,
-            self.f_farlineid,
-            self.w_assemblycenter,
-            self.w_originaladdress,
-            self.w_birthcountry,
-            self.w_fatheroccup,
-            self.w_fatheroccupcat,
-            self.w_yearsschooljapan,
-            self.w_gradejapan,
-            self.w_schooldegree,
-            self.w_yearofusarrival,
-            self.w_timeinjapan,
-            self.w_notimesinjapan,
-            self.w_ageinjapan,
-            self.w_militaryservice,
-            self.w_maritalstatus,
-            self.w_ethnicity,
-            self.w_birthplace,
-            self.w_citizenshipstatus,
-            self.w_highestgrade,
-            self.w_language,
-            self.w_religion,
-            self.w_occupqual1,
-            self.w_occupqual2,
-            self.w_occupqual3,
-            self.w_occuppotn1,
-            self.w_occuppotn2,
-            self.w_filenumber,
-        ]
-        self.fulltext = ' '.join([
-            f.lower() for f in fields if isinstance(f, str)
-        ])
+        return self.assemble_fulltext(FIELDS_PERSON)
+
+
+FIELDS_FARRECORD = [
+    'far_record_id', 'facility', 'original_order', 'family_number',
+    'far_line_id', 'last_name', 'first_name', 'other_names', 'date_of_birth',
+    'year_of_birth', 'sex', 'marital_status', 'citizenship',
+    'alien_registration', 'entry_type_code', 'entry_type', 'entry_category',
+    'entry_facility', 'pre_evacuation_address', 'pre_evacuation_state',
+    'date_of_original_entry', 'departure_type_code', 'departure_type',
+    'departure_category', 'departure_facility', 'departure_date',
+    'departure_state', 'camp_address_original', 'camp_address_block',
+    'camp_address_barracks', 'camp_address_room', 'reference', 'original_notes',
+    'person', 'timestamp',
+]
+
+class FarRecord(Record):
+    """FarRecord model
+    """
+    far_record_id           = dsl.Keyword()
+    facility                = dsl.Keyword()
+    original_order          = dsl.Keyword()
+    family_number           = dsl.Keyword()
+    far_line_id             = dsl.Keyword()
+    last_name               = dsl.Keyword()
+    first_name              = dsl.Keyword()
+    other_names             = dsl.Keyword()
+    date_of_birth           = dsl.Keyword()
+    year_of_birth           = dsl.Keyword()
+    sex                     = dsl.Keyword()
+    marital_status          = dsl.Keyword()
+    citizenship             = dsl.Keyword()
+    alien_registration      = dsl.Keyword()
+    entry_type_code         = dsl.Keyword()
+    entry_type              = dsl.Keyword()
+    entry_category          = dsl.Keyword()
+    entry_facility          = dsl.Keyword()
+    pre_evacuation_address  = dsl.Keyword()
+    pre_evacuation_state    = dsl.Keyword()
+    date_of_original_entry  = dsl.Keyword()
+    departure_type_code     = dsl.Keyword()
+    departure_type          = dsl.Keyword()
+    departure_category      = dsl.Keyword()
+    departure_facility      = dsl.Keyword()
+    departure_date          = dsl.Keyword()
+    departure_state         = dsl.Keyword()
+    camp_address_original   = dsl.Keyword()
+    camp_address_block      = dsl.Keyword()
+    camp_address_barracks   = dsl.Keyword()
+    camp_address_room       = dsl.Keyword()
+    reference               = dsl.Keyword()
+    original_notes          = dsl.Keyword()
+    person                  = dsl.Keyword()
+    #timestamp               = dsl.DateTime()
+    
+    def __repr__(self):
+        return f'<FarRecord {self.far_record_id}>'
+    
+    @staticmethod
+    def from_dict(fieldnames, far_record_id, data):
+        """
+        @param fieldnames: list
+        @param far_record_id: str
+        @param data: dict
+        @returns: FarRecord
+        """
+        return Record.from_dict(FarRecord, fieldnames, far_record_id, data)
+    
+    @staticmethod
+    def from_hit(hit):
+        """Build FarRecord object from Elasticsearch hit
+        @param hit
+        @returns: FarRecord
+        """
+        return Record.from_hit(FarRecord, hit)
+     
+    @staticmethod
+    def field_values(field, es=None, index=None):
+        """Returns unique values and counts for specified field.
+        """
+        return Record.field_values(FarRecord, field, es, index)
+    
+    def assemble_fulltext(self):
+        """Assembles single fulltext search field from all string fields
+        """
+        return self.assemble_fulltext(FIELDS_FARRECORD)
+
+
+FIELDS_WRARECORD = [
+    'wra_record_id', 'facility', 'lastname', 'firstname', 'middleinitial',
+    'birthyear', 'gender', 'originalstate', 'familyno', 'individualno', 'notes',
+    'assemblycenter', 'originaladdress', 'birthcountry', 'fatheroccupus',
+    'fatheroccupabr', 'yearsschooljapan', 'gradejapan', 'schooldegree',
+    'yearofusarrival', 'timeinjapan', 'ageinjapan', 'militaryservice',
+    'martitalstatus', 'ethnicity', 'birthplace', 'citizenshipstatus',
+    'highestgrade', 'language', 'religion', 'occupqual1', 'occupqual2',
+    'occupqual3', 'occupotn1', 'occupotn2', 'wra_filenumber', 'person',
+    'timestamp',
+]
+
+class WraRecord(Record):
+    """WraRecord model
+    """
+    wra_record_id     = dsl.Integer()
+    facility          = dsl.Keyword()
+    lastname          = dsl.Keyword()
+    firstname         = dsl.Keyword()
+    middleinitial     = dsl.Keyword()
+    birthyear         = dsl.Keyword()
+    gender            = dsl.Keyword()
+    originalstate     = dsl.Keyword()
+    familyno          = dsl.Keyword()
+    individualno      = dsl.Keyword()
+    notes             = dsl.Keyword()
+    assemblycenter    = dsl.Keyword()
+    originaladdress   = dsl.Keyword()
+    birthcountry      = dsl.Keyword()
+    fatheroccupus     = dsl.Keyword()
+    fatheroccupabr    = dsl.Keyword()
+    yearsschooljapan  = dsl.Keyword()
+    gradejapan        = dsl.Keyword()
+    schooldegree      = dsl.Keyword()
+    yearofusarrival   = dsl.Keyword()
+    timeinjapan       = dsl.Keyword()
+    ageinjapan        = dsl.Keyword()
+    militaryservice   = dsl.Keyword()
+    martitalstatus    = dsl.Keyword()
+    ethnicity         = dsl.Keyword()
+    birthplace        = dsl.Keyword()
+    citizenshipstatus = dsl.Keyword()
+    highestgrade      = dsl.Keyword()
+    language          = dsl.Keyword()
+    religion          = dsl.Keyword()
+    occupqual1        = dsl.Keyword()
+    occupqual2        = dsl.Keyword()
+    occupqual3        = dsl.Keyword()
+    occupotn1         = dsl.Keyword()
+    occupotn2         = dsl.Keyword()
+    wra_filenumber    = dsl.Keyword()
+    person            = dsl.Keyword()
+    #timestamp         = dsl.DateTime()
+    
+    def __repr__(self):
+        return f'<WraRecord {self.wra_record_id}>'
+    
+    @staticmethod
+    def from_dict(fieldnames, wra_record_id, data):
+        """
+        @param fieldnames: list
+        @param wra_record_id: str
+        @param data: dict
+        @returns: WraRecord
+        """
+        return Record.from_dict(WraRecord, fieldnames, wra_record_id, data)
+    
+    @staticmethod
+    def from_hit(hit):
+        """Build WraRecord object from Elasticsearch hit
+        @param hit
+        @returns: WraRecord
+        """
+        return Record.from_hit(WraRecord, hit)
+     
+    @staticmethod
+    def field_values(field, es=None, index=None):
+        """Returns unique values and counts for specified field.
+        """
+        return Record.field_values(WraRecord, field, es, index)
+    
+    def assemble_fulltext(self):
+        """Assembles single fulltext search field from all string fields
+        """
+        return self.assemble_fulltext(FIELDS_WRARECORD)
