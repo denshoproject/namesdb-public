a73c3a60186db38dba7f3256368afe7118953684
Geoffrey Jost <geoffrey@jostwebwerks.com>
2022-05-19 10:26:51 -0700

    Rename `namesdb_public` to `namespub`

--- namespub/templatetags/namespub_tags.py
+++ namespub/templatetags/namespub_tags.py
@@ -7,14 +7,14 @@ register = template.Library()
 def record( record ):
     if record.get('model'):
         t = template.loader.get_template(
-            f'namesdb_public/list-object-{record["model"]}.html'
+            f'namespub/list-object-{record["model"]}.html'
         )
     else:
-        t = template.loader.get_template('namesdb_public/list-object.html')
+        t = template.loader.get_template('namespub/list-object.html')
     return t.render({'record':record})
 	
 def names_paginate( paginator ):
-    t = template.loader.get_template('namesdb_public/names-paginate.html')
+    t = template.loader.get_template('namespub/names-paginate.html')
     return t.render({'paginator':paginator})
 
 register.simple_tag(record)
