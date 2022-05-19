0060a383bcad744f8ff14051d6a1019ab97357f7
Geoffrey Jost <geoffrey@jostwebwerks.com>
2022-05-19 15:51:15 -0700

    views: Fix url name typo

--- namessite/namespub/views.py
+++ namessite/namespub/views.py
@@ -76,7 +76,7 @@ def search_ui(request, model=None):
         highlight_fields = models.HIGHLIGHT_FIELDS_WRARECORD
     
     api_url = '%s?%s' % (
-        internal_url(request, reverse('names-api-search')),
+        internal_url(request, reverse('namesdb-api-search')),
         request.META['QUERY_STRING']
     )
     context = {
